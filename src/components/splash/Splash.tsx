b233f4d5a7e146d5e90244b4091c6b16ce70092c
Aryasuta <aryasutasaputra@gmail.com>
2022-02-21 14:41:38 +0700

    Tambah Fadeout

--- src/components/splash/Splash.tsx
+++ src/components/splash/Splash.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import './Splash.css';
 
 interface SplashProps { }
 
 const Splash: React.FC<SplashProps> = () => {
+
+  const[showSplash, changeSplash] = useState({
+    showSplash: "splashContainer"
+  });
+
+  useEffect(() =>{
+    setTimeout(() => {
+      changeSplash({
+        showSplash: "splashContainer fadeout"
+      })
+    }, 2000);
+  })
+
   return (
-    <div className="splashContainer">
-  
+    <div className={showSplash.showSplash}>
     </div>
   );
 };
